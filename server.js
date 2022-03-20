69f326b3ea4a12a0d7942a4edde9eb58bc984004
lj787448952 <lj787448952@gmail.com>
2022-03-20 14:55:30 +0800

    fix env.PORT

--- server.js
+++ server.js
@@ -104,4 +104,4 @@ const requestListener = (req, res) => {
 
 }
 const server = http.createServer(requestListener);
-server.listen(process.env.port || 3000);
+server.listen(process.env.PORT || 3000);
